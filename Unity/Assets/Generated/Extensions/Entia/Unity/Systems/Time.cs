28f3a2c413105ef564eec12622b268102bd3dced
Magicolo <straty91@hotmail.com>
2019-02-13 19:01:47 -0500

    - updated generated

--- Unity/Assets/Generated/Extensions/Entia/Unity/Systems/Time.cs
+++
@@ -1,8 +0,0 @@
-namespace Entia.Unity.Systems
-{
-	[global::Entia.Unity.Generation.GeneratedAttribute(Type = typeof(global::Entia.Unity.Systems.Time), Link = "", Path = new string[] { "Entia", "Unity", "Systems", "Time" })]
-	public static class TimeExtensions
-	{
-
-	}
-}
---
+++ Unity/Assets/Generated/Extensions/Entia/Unity/Systems/UpdateTime.cs
@@ -0,0 +1,8 @@
+namespace Entia.Unity.Systems
+{
+	[global::Entia.Unity.Generation.GeneratedAttribute(Type = typeof(global::Entia.Unity.Systems.UpdateTime), Link = "", Path = new string[] { "Entia", "Unity", "Systems", "UpdateTime" })]
+	public static class UpdateTimeExtensions
+	{
+
+	}
+}
