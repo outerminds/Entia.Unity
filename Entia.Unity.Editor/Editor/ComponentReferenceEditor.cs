f0ca6dfdd6532d578e44af5bccea8c20cdfa7446
Magicolo <straty91@hotmail.com>
2019-05-23 15:43:37 -0400

    - added generic versions for component/resource editors

--- Entia.Unity.Editor/Editor/ComponentReferenceEditor.cs
+++ Entia.Unity.Editor/Editor/ComponentReferenceEditor.cs
@@ -4,16 +4,23 @@ using UnityEditor;
 
 namespace Entia.Unity.Editor
 {
-    [CustomEditor(typeof(ComponentReference), true, isFallback = true), CanEditMultipleObjects]
-    public class ComponentReferenceEditor : UnityEditor.Editor
+    public abstract class ComponentReferenceEditor<T> : UnityEditor.Editor where T : class, IComponentReference
     {
-        public IComponentReference Target => target as IComponentReference;
-        public IEnumerable<IComponentReference> Targets => targets.OfType<IComponentReference>();
+        public T Target => target as T;
+        public IEnumerable<T> Targets => targets.OfType<T>();
 
         public override void OnInspectorGUI()
         {
             using (ReferenceUtility.Component(serializedObject, Targets, out var property))
-                while (property.NextVisible(false)) EditorGUILayout.PropertyField(property, true);
+                OnComponentInspectorGUI(property);
+        }
+
+        protected virtual void OnComponentInspectorGUI(SerializedProperty property)
+        {
+            while (property.NextVisible(false)) EditorGUILayout.PropertyField(property, true);
         }
     }
+
+    [CustomEditor(typeof(ComponentReference), true, isFallback = true), CanEditMultipleObjects]
+    public class ComponentReferenceEditor : ComponentReferenceEditor<IComponentReference> { }
 }
--- Entia.Unity.Editor/Editor/ResourceReferenceEditor.cs
+++ Entia.Unity.Editor/Editor/ResourceReferenceEditor.cs
@@ -4,8 +4,7 @@ using UnityEditor;
 
 namespace Entia.Unity.Editor
 {
-    [CustomEditor(typeof(ResourceReference), true, isFallback = true), CanEditMultipleObjects]
-    public class ResourceReferenceEditor : UnityEditor.Editor
+    public abstract class ResourceReferenceEditor<T> : UnityEditor.Editor where T : class, IResourceReference
     {
         public IResourceReference Target => target as IResourceReference;
         public IEnumerable<IResourceReference> Targets => targets.OfType<IResourceReference>();
@@ -13,7 +12,15 @@ namespace Entia.Unity.Editor
         public override void OnInspectorGUI()
         {
             using (ReferenceUtility.Resource(serializedObject, Targets, out var property))
-                while (property.NextVisible(false)) EditorGUILayout.PropertyField(property, true);
+                OnResourceInspectorGUI(property);
+        }
+
+        protected virtual void OnResourceInspectorGUI(SerializedProperty property)
+        {
+            while (property.NextVisible(false)) EditorGUILayout.PropertyField(property, true);
         }
     }
+
+    [CustomEditor(typeof(ResourceReference), true, isFallback = true), CanEditMultipleObjects]
+    public class ResourceReferenceEditor : ResourceReferenceEditor<IResourceReference> { }
 }
